cd1728aeefc4f03ffc1f84f8d6f1ec302e21ad09
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-06-14 21:25:17 -0700

    linear_search done

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,10 @@
 def linear_search(arr, target):
     # Your code here
-
+    if len(arr) == 0:
+        return -1
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
 
     return -1   # not found
 
@@ -10,5 +14,30 @@ def binary_search(arr, target):
 
     # Your code here
 
-
     return -1  # not found
+
+
+# lo = 0
+# hi = len(arr)
+# while lo < hi:
+#    mid = (lo + hi) // 2
+
+#    if arr[mid] == target:
+#       return True
+#    elif target < arr[mid]:
+#       hi = mid
+#    else:
+#       lo = mid + 1
+
+# return False
+
+# def insertion_sort(arr):
+#    for i in range(1, len(arr)):
+#       temp = arr[i]
+#       j = i
+#       while j > 0 and temp < arr[j - 1]:
+#          arr[i] = arr[j - 1]
+#          j -= 1
+
+#       arr[j] = temp
+#    return arr
